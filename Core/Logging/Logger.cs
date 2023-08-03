7c01c52831283755d64240ff0f7ebf8a697f696f
trofimoff <trofimoff@skbkontur.ru>
2023-08-04 00:04:58 +0400

    fix console logger

--- Core/Logging/Logger.cs
+++ Core/Logging/Logger.cs
@@ -6,16 +6,16 @@ internal sealed class ConsoleLogger : ILogger
 {
     public void Info(string message)
     {
-        Console.WriteLine(@$"[Info]\t {message}");
+        Console.WriteLine($"[{DateTime.Now}] [Info] : {message}");
     }
 
     public void Warn(string message)
     {
-        Console.WriteLine(@$"[Warn]\t {message}");
+        Console.WriteLine($"[{DateTime.Now}] [Warn] : {message}");
     }
 
     public void Error(string message)
     {
-        Console.WriteLine(@$"[Error]\t {message}");
+        Console.WriteLine($"[{DateTime.Now}] [Error] : {message}");
     }
 }
