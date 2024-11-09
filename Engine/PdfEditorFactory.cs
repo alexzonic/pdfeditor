a826ac0bbe9ba7c2f271507654543fe9a39e48b1
trofimoff <alexzonic@yandex.ru>
2024-11-09 13:59:11 +0400

    remove legacy docs

--- Engine/PdfEditorFactory.cs
+++ Engine/PdfEditorFactory.cs
@@ -5,8 +5,6 @@ public static class PdfEditorFactory
     /// <summary>
     /// Создает экземпляр IPdfEditor
     /// </summary>
-    /// <param name="sourceFilePath">Путь до оригинального pdf файла</param>
-    /// <param name="editedFileName">Имя обновленного pdf файла. По умолчанию 'updated_имя-оригинала'</param>
     /// <returns>Instance of IPdfEditor</returns>
     public static IPdfEditor Create() => new PdfEditor();
 }
